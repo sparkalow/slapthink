3174239a81639a183229fdce76d00c61c7aea24d
sparkalow <brian@slapthink.net>
2024-04-18 09:33:02 -0500

    Add cachebuster filter

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,20 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const responsiveImgs = require("./src/_plugins/responsive.images.js");
 
 module.exports = function (eleventyConfig) {
+    const now = new Date();
     process.env.TZ = "UTC";
 
     eleventyConfig.setTemplateFormats("md,njk,html");
+
+    eleventyConfig.addFilter("cachebust", (url) => {
+        const params = new URLSearchParams();
+
+        try {
+            params.set("v", now.getTime());
+        } catch (error) {}
+
+        return `${url}?${params}`;
+    });
 
     //plugins
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -13,20 +24,23 @@ module.exports = function (eleventyConfig) {
     // Assets
     // eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/img");
-    eleventyConfig.addPassthroughCopy({"src/public": "/"});
+    eleventyConfig.addPassthroughCopy({ "src/public": "/" });
 
     eleventyConfig.setServerOptions({
-        watch: ["_site/*.css"]
-    })
+        watch: ["_site/*.css"],
+    });
 
     eleventyConfig.addAsyncShortcode("image", responsiveImgs.rimage);
 
     // collections
     eleventyConfig.addCollection("sites", function (collectionApi) {
-        return collectionApi.getFilteredByGlob("./src/sites/*.md").sort((a, b) => b.data.order - a.data.order).reverse();
+        return collectionApi
+            .getFilteredByGlob("./src/sites/*.md")
+            .sort((a, b) => b.data.order - a.data.order)
+            .reverse();
     });
 
     return {
-        dir: {input: "src", output: "_site"},
+        dir: { input: "src", output: "_site" },
     };
 };
